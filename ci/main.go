0b021b0b3a92af12f284f07fa89540883fba8645
Csaba Almasi <almasicsaba89@gmail.com>
2024-04-04 16:34:05 +0200

    harbor 2 pipelines

---
+++ ci/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+
+	// use a golang:1.19 container
+	// get version
+	// execute
+	golang := client.Container().From("golang:1.21").WithExec([]string{"go", "version"})
+
+	version, err := golang.Stdout(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// print output
+	slog.Info("Hello from Dagger!", "version", version)
+}
---
+++ go.mod
@@ -0,0 +1,16 @@
+module container-registry.com/harbor-satelite
+
+go 1.22.2
+
+require dagger.io/dagger v0.10.3
+
+require (
+	github.com/99designs/gqlgen v0.17.31 // indirect
+	github.com/Khan/genqlient v0.6.0 // indirect
+	github.com/adrg/xdg v0.4.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
+	golang.org/x/sync v0.6.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
