09419ce8f919410983420f6045b4aec41c7f70ad
Mehul-Kumar-27 <mehul2002kumar@gmail.com>
2024-10-03 00:23:45 +0530

    adding new format of the state file

--- internal/state/artifact.go
+++ internal/state/artifact.go
@@ -1,29 +1,34 @@
 package state
 
+import "reflect"
+
 // ArtifactReader defines an interface for reading artifact data
 type ArtifactReader interface {
-	// GetRepository returns the repository of the artifact
 	GetRepository() string
-	// GetTag returns the tag of the artifact
-	GetTag() string
-	// GetHash returns the hash of the artifact
-	GetHash() string
-	// HasChanged returns true if the artifact has changed
+	GetTags() []string
+	GetDigest() string
+	GetType() string
+	IsDeleted() bool
 	HasChanged(newArtifact ArtifactReader) bool
 }
 
 // Artifact represents an artifact object in the registry
 type Artifact struct {
-	Repository string `json:"repository"`
-	Tag        string `json:"tag"`
-	Hash       string `json:"hash"`
+	Deleted    bool     `json:"deleted"`
+	Repository string   `json:"repository"`
+	Tags       []string `json:"tag"`
+	Digest     string   `json:"digest"`
+	Type       string   `json:"type"`
 }
 
-func NewArtifact(repository, tag, hash string) ArtifactReader {
+// NewArtifact creates a new Artifact object
+func NewArtifact(deleted bool, repository string, tags []string, digest, artifactType string) ArtifactReader {
 	return &Artifact{
+		Deleted:    deleted,
 		Repository: repository,
-		Tag:        tag,
-		Hash:       hash,
+		Tags:       tags,
+		Digest:     digest,
+		Type:       artifactType,
 	}
 }
 
@@ -31,14 +36,44 @@ func (a *Artifact) GetRepository() string {
 	return a.Repository
 }
 
-func (a *Artifact) GetTag() string {
-	return a.Tag
+func (a *Artifact) GetTags() []string {
+	return a.Tags
 }
 
-func (a *Artifact) GetHash() string {
-	return a.Hash
+func (a *Artifact) GetDigest() string {
+	return a.Digest
 }
 
+func (a *Artifact) GetType() string {
+	return a.Type
+}
+
+func (a *Artifact) IsDeleted() bool {
+	return a.Deleted
+}
+
+// HasChanged compares the current artifact with another to determine if there are any changes
 func (a *Artifact) HasChanged(newArtifact ArtifactReader) bool {
-	return a.GetHash() != newArtifact.GetHash()
+	// Compare the digest (hash)
+	if a.GetDigest() != newArtifact.GetDigest() {
+		return true
+	}
+
+	// Compare the repository
+	if a.GetRepository() != newArtifact.GetRepository() {
+		return true
+	}
+
+	// Compare the tags (order-agnostic comparison)
+	if !reflect.DeepEqual(a.GetTags(), newArtifact.GetTags()) {
+		return true
+	}
+
+	// Compare the deletion status
+	if a.IsDeleted() != newArtifact.IsDeleted() {
+		return true
+	}
+
+	// No changes detected
+	return false
 }
