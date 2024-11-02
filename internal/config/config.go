d222265c8e54f5f90637d66bf618d4daca259d16
bupd <bupdprasanth@gmail.com>
2024-11-02 15:13:58 +0530

    fix satellite config
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,8 +2,12 @@ package config
 
 import (
 	"encoding/json"
-
+	"fmt"
+	"net/http"
 	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/spf13/viper"
 )
 
 var appConfig *Config
@@ -41,9 +45,72 @@ type StateConfig struct {
 }
 
 type Config struct {
-	StateConfig     StateConfig     `json:"state_config,omitempty"`
-	LocalJsonConfig LocalJsonConfig `json:"environment_variables,omitempty"`
-	ZotUrl          string          `json:"zot_url,omitempty"`
+	log_level          string
+	own_registry       bool
+	own_registry_adr   string
+	own_registry_port  string
+	zot_config_path    string
+	ground_control_url string
+	input              string
+	zot_url            string
+	registry           string
+	repository         string
+	user_input         string
+	scheme             string
+	api_version        string
+	image              string
+	token              string
+	states             []string
+	harbor_password    string
+	harbor_username    string
+	env                string
+	use_unsecure       bool
+}
+
+type ZtrResult struct {
+	States []string `json:"states"`
+	Auth   Account  `json:"auth"`
+}
+
+type Account struct {
+	Name     string `json:"name"`
+	Secret   string `json:"secret"`
+	Registry string `json:"registry"`
+}
+
+// fetch data from Ground Control, parse it
+func ztr(gcUrl string) (*ZtrResult, error) {
+	// Get environment variables
+	token := os.Getenv("TOKEN")
+
+	// Construct the request URL
+	url := fmt.Sprintf("%s/satellites/ztr/%s", gcUrl, token)
+
+	// Make the GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch data from Ground Control: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Check for a successful response
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected response from Ground Control: %v", resp.Status)
+	}
+
+	// Decode the JSON response
+	var res ZtrResult
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, fmt.Errorf("failed to decode JSON response: %v", err)
+	}
+
+	viper.Set("auth.name", res.Auth.Name)         // Set auth name in config
+	viper.Set("auth.secret", res.Auth.Secret)     // Set auth secret in config
+	viper.Set("auth.registry", res.Auth.Registry) // Set auth registry in config
+	viper.Set("states", res.States)               // Set states array in config
+	viper.WriteConfigAs("config.gen.json")
+
+	return &res, nil
 }
 
 type Job struct {
@@ -51,19 +118,159 @@ type Job struct {
 	Schedule string `json:"schedule"`
 }
 
-// ParseConfigFromJson parses a JSON string into a Config struct. Returns an error if the JSON is invalid
-func ParseConfigFromJson(jsonData string) (*Config, error) {
-	var config Config
-	err := json.Unmarshal([]byte(jsonData), &config)
-	if err != nil {
-		return nil, err
+func GetOwnRegistry() bool {
+	return AppConfig.own_registry
+}
+
+func GetOwnRegistryAdr() string {
+	return AppConfig.own_registry_adr
+}
+
+func GetOwnRegistryPort() string {
+	return AppConfig.own_registry_port
+}
+
+func GetZotConfigPath() string {
+	return AppConfig.zot_config_path
+}
+
+func GetGroundControlURL() string {
+	return AppConfig.ground_control_url
+}
+
+func GetInput() string {
+	return AppConfig.input
+}
+
+func SetZotURL(url string) {
+	AppConfig.zot_url = url
+}
+
+func GetZotURL() string {
+	return AppConfig.zot_url
+}
+
+func SetRegistry(registry string) {
+	AppConfig.registry = registry
+}
+
+func GetRegistry() string {
+	return AppConfig.registry
+}
+
+func SetRepository(repository string) {
+	AppConfig.repository = repository
+}
+
+func GetRepository() string {
+	return AppConfig.repository
+}
+
+func SetUserInput(user_input string) {
+	AppConfig.user_input = user_input
+}
+
+func GetUserInput() string {
+	return AppConfig.user_input
+}
+
+func SetScheme(scheme string) {
+	AppConfig.scheme = scheme
+}
+
+func GetScheme() string {
+	return AppConfig.scheme
+}
+
+func SetAPIVersion(api_version string) {
+	AppConfig.api_version = api_version
+}
+
+func GetAPIVersion() string {
+	return AppConfig.api_version
+}
+
+func SetImage(image string) {
+	AppConfig.image = image
+}
+
+func GetImage() string {
+	return AppConfig.image
+}
+
+func UseUnsecure() bool {
+	return AppConfig.use_unsecure
+}
+
+func GetToken() string {
+	return AppConfig.token
+}
+
+func GetStates() []string {
+	return AppConfig.states
+}
+
+func GetHarborPassword() string {
+	return AppConfig.harbor_password
+}
+
+func GetHarborUsername() string {
+	return AppConfig.harbor_username
+}
+
+func LoadConfig() (*Config, error) {
+	// Check if config.gen.json exists
+	if _, err := os.Stat("config.gen.json"); err == nil {
+		// If config.gen.json exists, set it as the configuration file
+		viper.SetConfigFile("config.gen.json")
+		fmt.Println("Using config.gen.json for configuration.")
+	} else if os.IsNotExist(err) {
+		// fall back to config.toml
+		viper.SetConfigName("config")
+		viper.SetConfigType("toml")
+		viper.AddConfigPath(".")
+
+		gcURL := viper.GetString("ground_control_url")
+		// Call ztr function to fetch and set the necessary configuration
+		_, err := ztr(gcURL)
+		if err != nil {
+			return nil, fmt.Errorf("Error in ztr function: %v\n", err)
+		}
+	} else {
+		return nil, fmt.Errorf("Error checking config file: %v\n", err)
 	}
-	return &config, nil
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading config file at path '%s': %w", viper.ConfigFileUsed(), err)
+	}
+
+	var use_unsecure bool
+	if os.Getenv("USE_UNSECURE") == "true" {
+		use_unsecure = true
+	} else {
+		use_unsecure = false
+	}
+
+	return &Config{
+		log_level:          viper.GetString("log_level"),
+		own_registry:       viper.GetBool("bring_own_registry"),
+		own_registry_adr:   viper.GetString("own_registry_adr"),
+		own_registry_port:  viper.GetString("own_registry_port"),
+		zot_config_path:    viper.GetString("zotConfigPath"),
+		ground_control_url: viper.GetString("ground_control_url"),
+		input:              viper.GetString("url_or_file"),
+		token:              os.Getenv("TOKEN"),
+		states:             viper.GetStringSlice("states"),
+		harbor_password:    viper.GetString("auth.secret"),
+		harbor_username:    viper.GetString("auth.name"),
+		env:                os.Getenv("ENV"),
+		zot_url:            os.Getenv("ZOT_URL"),
+		use_unsecure:       use_unsecure,
+	}, nil
 }
 
 // ReadConfigData reads the data from the specified path. Returns an error if the file does not exist or is a directory
 func ReadConfigData(configPath string) ([]byte, error) {
-
 	fileInfo, err := os.Stat(configPath)
 	if err != nil {
 		return nil, err
@@ -134,6 +341,7 @@ func UpdateStateAuthConfig(name, registry, secret string, states []string) {
 	appConfig.StateConfig.States = states
 	WriteConfig(DefaultConfigPath)
 }
+
 func WriteConfig(configPath string) error {
 	data, err := json.MarshalIndent(appConfig, "", "  ")
 	if err != nil {
--- main.go
+++ main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"container-registry.com/harbor-satellite/cmd"
+	"container-registry.com/harbor-satellite/internal/config"
+	"container-registry.com/harbor-satellite/internal/images"
+	"container-registry.com/harbor-satellite/internal/replicate"
+	"container-registry.com/harbor-satellite/internal/satellite"
+	"container-registry.com/harbor-satellite/internal/server"
+	"container-registry.com/harbor-satellite/internal/store"
+	"container-registry.com/harbor-satellite/internal/utils"
+	"container-registry.com/harbor-satellite/logger"
+	"golang.org/x/sync/errgroup"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/rs/zerolog"
 )
 
 func main() {
@@ -13,4 +28,129 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func run() error {
+	// Initialize Config and Logger
+	if err := config.InitConfig(); err != nil {
+		return fmt.Errorf("error initializing config: %w", err)
+	}
+
+	ctx, cancel := setupContext()
+	defer cancel()
+
+	g, ctx := errgroup.WithContext(ctx)
+	ctx = logger.AddLoggerToContext(ctx, config.GetLogLevel())
+	log := logger.FromContext(ctx)
+	log.Info().Msg("Satellite starting")
+
+	// Set up router and app
+	app := setupServerApp(ctx, log)
+	app.SetupRoutes()
+	app.SetupServer(g)
+
+	// Handle registry setup
+	if err := handleRegistrySetup(g, log, cancel); err != nil {
+		return err
+	}
+
+	// Process Input (file or URL)
+	fetcher, err := processInput(ctx, log)
+	if err != nil {
+		return err
+	}
+
+	ctx, storer := store.NewInMemoryStore(ctx, fetcher)
+	replicator := replicate.NewReplicator(ctx)
+	satelliteService := satellite.NewSatellite(ctx, storer, replicator)
+
+	g.Go(func() error {
+		return satelliteService.Run(ctx)
+	})
+
+	log.Info().Msg("Satellite running")
+	return g.Wait()
+}
+
+func setupContext() (context.Context, context.CancelFunc) {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	return ctx, cancel
+}
+
+func setupServerApp(ctx context.Context, log *zerolog.Logger) *server.App {
+	router := server.NewDefaultRouter("/api/v1")
+	router.Use(server.LoggingMiddleware)
+
+	return server.NewApp(
+		router,
+		ctx,
+		log,
+		config.AppConfig,
+		&server.MetricsRegistrar{},
+		&server.DebugRegistrar{},
+		&satellite.SatelliteRegistrar{},
+	)
+}
+
+func handleRegistrySetup(g *errgroup.Group, log *zerolog.Logger, cancel context.CancelFunc) error {
+	if config.GetOwnRegistry() {
+		if err := utils.HandleOwnRegistry(); err != nil {
+			log.Error().Err(err).Msg("Error handling own registry")
+			return err
+		}
+	} else {
+		log.Info().Msg("Launching default registry")
+		g.Go(func() error {
+			if err := utils.LaunchDefaultZotRegistry(); err != nil {
+				log.Error().Err(err).Msg("Error launching default registry")
+				cancel()
+				return err
+			}
+			cancel()
+			return nil
+		})
+	}
+	return nil
+}
+
+func processInput(ctx context.Context, log *zerolog.Logger) (store.ImageFetcher, error) {
+	input := config.GetInput()
+	if !utils.IsValidURL(input) {
+		log.Info().Msg("Input is not a valid URL, checking if it is a file path")
+		if err := validateFilePath(config.GetInput(), log); err != nil {
+			return nil, err
+		}
+		return setupFileFetcher(ctx, log)
+	}
+
+	log.Info().Msg("Input is a valid URL")
+	fetcher := store.RemoteImageListFetcher(ctx, input)
+	utils.SetUrlConfig(input)
+	return fetcher, nil
+}
+
+func validateFilePath(path string, log *zerolog.Logger) error {
+	if utils.HasInvalidPathChars(path) {
+		log.Error().Msg("Path contains invalid characters")
+		return fmt.Errorf("invalid file path: %s", path)
+	}
+	if err := utils.GetAbsFilePath(path); err != nil {
+		log.Error().Err(err).Msg("No file found")
+		return fmt.Errorf("no file found: %s", path)
+	}
+	return nil
+}
+
+func setupFileFetcher(ctx context.Context, log *zerolog.Logger) (store.ImageFetcher, error) {
+	fetcher := store.FileImageListFetcher(ctx, config.GetInput())
+	var imagesList images.ImageList
+	if err := utils.ParseImagesJsonFile(config.GetInput(), &imagesList); err != nil {
+		log.Error().Err(err).Msg("Error parsing images.json file")
+		return nil, err
+	}
+	if err := utils.SetRegistryEnvVars(imagesList); err != nil {
+		log.Error().Err(err).Msg("Error setting registry environment variables")
+		return nil, err
+	}
+	return fetcher, nil
 }
