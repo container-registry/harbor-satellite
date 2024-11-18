6574d77c8eb4d8cea5a364f3b4801a115b027028
Mehul-Kumar-27 <202151092@iiitvadodara.ac.in>
2024-11-19 03:22:24 +0530

    adding event driven architecture changes

--- internal/scheduler/process.go
+++ internal/scheduler/process.go
@@ -1,6 +1,10 @@
 package scheduler
 
-import "context"
+import (
+	"context"
+
+	"github.com/robfig/cron/v3"
+)
 
 // Process represents a process that can be scheduled
 type Process interface {
@@ -8,7 +12,10 @@ type Process interface {
 	Execute(ctx context.Context) error
 
 	// GetID returns the unique GetID of the process
-	GetID() uint64
+	GetID() cron.EntryID
+
+	// SetID sets the unique GetID of the process
+	SetID(id cron.EntryID)
 
 	// GetName returns the name of the process
 	GetName() string
@@ -18,4 +25,10 @@ type Process interface {
 
 	// IsRunning returns true if the process is running
 	IsRunning() bool
+
+	// CanExecute returns true if all conditions are fulfilled to execute the process
+	CanExecute(ctx context.Context) (bool, string)
+
+	// AddEventBroker adds the event broker to the process here the process could subscribe to the events
+	AddEventBroker(eventBroker *EventBroker, ctx context.Context)
 }
