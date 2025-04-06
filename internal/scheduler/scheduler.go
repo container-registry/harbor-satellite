3a6a3414e49750e1e21adcc7e14840bc39f328f5
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-04-06 21:35:39 +0530

    add todo

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -104,6 +104,9 @@ func (s *BasicScheduler) Schedule(process Process) error {
 	s.logger.Info().Msgf("Process %s scheduled with cron expression %s", process.GetName(), process.GetCronExpr())
 	process.SetID(cronEntryId)
 
+    // TODO: a more comprehensive fix to running the process at startup would require a
+    // large refactor of the process scheduling logic, to ensure that ZTR is performed before
+    // attempting to execute any of the other processes.
 	go func() {
 		if !process.IsRunning() {
 			if err := s.executeProcess(process); err != nil {
