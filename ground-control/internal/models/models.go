3ff89a42eb8c93ba7238fea46413710020211a26
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-04-08 12:28:06 +0530

    add model

--- ground-control/internal/models/models.go
+++ ground-control/internal/models/models.go
@@ -9,6 +9,27 @@ type StateArtifact struct {
 	Registry  string     `json:"registry,omitempty"`
 	Artifacts []Artifact `json:"artifacts,omitempty"`
 }
+
+//TODO: move config to a common pkg for both satellite and ground control
+type LocalRegistryConfig struct {
+	URL              string `json:"url"`
+	UserName         string `json:"username"`
+	Password         string `json:"password"`
+	BringOwnRegistry bool   `json:"bring_own_registry"`
+}
+
+// LocalJsonConfig is a struct that holds the configs that are passed as environment variables
+type LocalJsonConfig struct {
+	GroundControlURL          string              `json:"ground_control_url"`
+	LogLevel                  string              `json:"log_level"`
+	UseUnsecure               bool                `json:"use_unsecure"`
+	ZotConfigPath             string              `json:"zot_config_path"`
+	StateReplicationInterval  string              `json:"state_replication_interval"`
+	UpdateConfigInterval      string              `json:"update_config_interval"`
+	RegisterSatelliteInterval string              `json:"register_satellite_interval"`
+	LocalRegistryConfig       LocalRegistryConfig `json:"local_registry"`
+}
+
 type Artifact struct {
 	Repository string   `json:"repository,omitempty"`
 	Tag        []string `json:"tag,omitempty"`
