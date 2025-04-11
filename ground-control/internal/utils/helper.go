fa446274bbb980cd1e9bef7148913a65b32a3259
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-04-11 20:48:23 +0530

    add a context for creation of config artifact

--- ground-control/internal/utils/helper.go
+++ ground-control/internal/utils/helper.go
@@ -145,8 +145,8 @@ func CreateStateArtifact(stateArtifact *m.StateArtifact) error {
 	return nil
 }
 
-// Create State Artifact for group
-func CreateConfigStateArtifact(configObject *m.ConfigObject) error {
+// Create State Artifact for Config
+func CreateConfigStateArtifact(ctx context.Context, configObject *m.ConfigObject) error {
 	// Marshal the state artifact to JSON format
 	configData, err := json.Marshal(configObject.Config)
 	if err != nil {
@@ -169,7 +169,7 @@ func CreateConfigStateArtifact(configObject *m.ConfigObject) error {
 		Username: username,
 		Password: password,
 	})
-	options := []crane.Option{crane.WithAuth(auth)}
+	options := []crane.Option{crane.WithAuth(auth), crane.WithContext(ctx)}
 
 	// Construct the destination repository and strip protocol, if present
 	destinationRepo := AssembleConfigState(configObject.ConfigName)
