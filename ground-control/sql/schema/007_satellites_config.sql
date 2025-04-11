e3da2fd0104555cf1307671bd3f4474c11653dd0
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-04-11 20:40:58 +0530

    update set config route

--- ground-control/sql/schema/007_satellites_config.sql
+++ ground-control/sql/schema/007_satellites_config.sql
@@ -1,11 +1,9 @@
 -- +goose Up
 
 CREATE TABLE satellite_configs (
-  satellite_id INT REFERENCES satellites(id) ON DELETE CASCADE,
-  config_id INT REFERENCES configs(id) ON DELETE CASCADE,
-  PRIMARY KEY (satellite_id, config_id)
+  satellite_id INT PRIMARY KEY REFERENCES satellites(id) ON DELETE CASCADE,
+  config_id INT NOT NULL REFERENCES configs(id) ON DELETE CASCADE
 );
-
 -- +goose Down
 
 DROP TABLE satellite_configs;
