02d79beea3491578a53896f6be56a2d2ef81ea46
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-04-15 17:43:44 +0530

    improve sql schema and queries with best practices

--- ground-control/sql/queries/configs.sql
+++ ground-control/sql/queries/configs.sql
@@ -1,5 +1,5 @@
 -- name: CreateConfig :one
-INSERT INTO configs (config_name, registry_url, config, created_at, updated_at)
+INSERT INTO groundcontrol.configs (config_name, registry_url, config, created_at, updated_at)
 VALUES ($1, $2, $3, NOW(), NOW())
 ON CONFLICT (config_name)
 DO UPDATE SET
@@ -9,24 +9,24 @@ DO UPDATE SET
 RETURNING id, config_name, registry_url, config, created_at, updated_at;
 
 -- name: ListConfigs :many
-SELECT id, config_name, registry_url, config, created_at, updated_at FROM configs;
+SELECT id, config_name, registry_url, config, created_at, updated_at FROM groundcontrol.configs;
 
 -- name: GetConfigByID :one
-SELECT id, config_name, registry_url, config, created_at, updated_at FROM configs
+SELECT id, config_name, registry_url, config, created_at, updated_at FROM groundcontrol.configs
 WHERE id = $1;
 
 -- name: GetConfigByName :one
-SELECT id, config_name, registry_url, config, created_at, updated_at FROM configs
+SELECT id, config_name, registry_url, config, created_at, updated_at FROM groundcontrol.configs
 WHERE config_name = $1;
 
 -- name: DeleteConfig :exec
-DELETE FROM configs
+DELETE FROM groundcontrol.configs
 WHERE id = $1;
 
 -- name: GetRawConfigByID :one
-SELECT config FROM configs
+SELECT config FROM groundcontrol.configs
 WHERE id = $1;
 
 -- name: GetRawConfigByName :one
-SELECT config FROM configs
+SELECT config FROM groundcontrol.configs
 WHERE config_name = $1;
