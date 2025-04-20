cb7e3bdb89088622a860e3d605b8c24abe78db45
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-04-20 16:22:23 +0530

    simplify satellite

--- cmd/main.go
+++ cmd/main.go
@@ -5,9 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/container-registry/harbor-satellite/internal/logger"
 	"github.com/container-registry/harbor-satellite/internal/satellite"
-	"github.com/container-registry/harbor-satellite/internal/state"
 	"github.com/container-registry/harbor-satellite/internal/utils"
 	"github.com/container-registry/harbor-satellite/pkg/config"
 	"github.com/container-registry/harbor-satellite/registry"
@@ -34,7 +32,7 @@ func run() error {
 		return err
 	}
 
-	ctx, log := utils.InitLogger(ctx, warnings)
+	ctx, log := utils.InitLogger(ctx, cm.GetLogLevel(), warnings)
 
 	ctx, scheduler := utils.InitScheduler(ctx)
 
@@ -53,29 +51,26 @@ func run() error {
 	}
 	defer scheduler.Stop()
 
-	localRegistryConfig := state.NewRegistryConfig(config.GetRemoteRegistryURL(), config.GetRemoteRegistryUsername(), config.GetRemoteRegistryPassword())
-	sourceRegistryConfig := state.NewRegistryConfig(config.GetSourceRegistryURL(), config.GetSourceRegistryUsername(), config.GetSourceRegistryPassword())
-	satelliteService := satellite.NewSatellite(ctx, scheduler.GetSchedulerKey(), localRegistryConfig, sourceRegistryConfig, config.UseUnsecure(), config.GetState())
+	satelliteService := satellite.NewSatellite(scheduler.GetSchedulerKey(), cm)
 
-    // Write the config to disk, in case any values were enforced at runtime
-    if err := cm.WriteConfig(); err != nil {
-        log.Error().Err(err).Msg("Error writing config to disk")
-        return err
-    }
+	// Write the config to disk, in case any values were enforced at runtime
+	if err := cm.WriteConfig(); err != nil {
+		log.Error().Err(err).Msg("Error writing config to disk")
+		return err
+	}
 
 	wg.Go(func() error {
 		return satelliteService.Run(ctx)
 	})
-
 
 	return wg.Wait()
 }
 
 func handleRegistrySetup(g *errgroup.Group, log *zerolog.Logger, cancel context.CancelFunc, cm *config.ConfigManager) error {
 	log.Debug().Msg("Setting up local registry")
-	if config.GetOwnRegistry() {
+	if cm.GetOwnRegistry() {
 		log.Info().Msg("Configuring own registry")
-		if err := utils.HandleOwnRegistry(); err != nil {
+		if err := utils.HandleOwnRegistry(cm); err != nil {
 			log.Error().Err(err).Msg("Error handling own registry")
 			cancel()
 			return err
@@ -83,19 +78,19 @@ func handleRegistrySetup(g *errgroup.Group, log *zerolog.Logger, cancel context.
 	} else {
 		log.Info().Msg("Launching default registry")
 		var defaultZotConfig registry.ZotConfig
-		if err := registry.ReadZotConfig(config.GetZotConfigPath(), &defaultZotConfig); err != nil {
+		if err := registry.ReadZotConfig(cm.GetZotConfigPath(), &defaultZotConfig); err != nil {
 			log.Error().Err(err).Msg("Error launching default zot registry")
 			return fmt.Errorf("error reading config: %w", err)
 		}
 
 		// TODO: Is this code block necessary?
-		err := cm.With(config.SetLocalRegistryCredentials(config.RegistryCredentials{URL: config.URL(defaultZotConfig.GetRegistryURL())}))
+		err := cm.With(config.SetLocalRegistryURL(defaultZotConfig.GetRegistryURL()))
 		if err != nil {
 			return fmt.Errorf("error setting RemoteRegistryURL")
 		}
 
 		g.Go(func() error {
-			if err := registry.LaunchRegistry(config.GetZotConfigPath()); err != nil {
+			if err := registry.LaunchRegistry(cm.GetZotConfigPath()); err != nil {
 				log.Error().Err(err).Msg("Error launching default zot registry")
 				cancel()
 				return fmt.Errorf("error launching default zot registry: %w", err)
--- internal/satellite/satellite.go
+++ internal/satellite/satellite.go
@@ -7,62 +7,55 @@ import (
 	"github.com/container-registry/harbor-satellite/internal/notifier"
 	"github.com/container-registry/harbor-satellite/internal/scheduler"
 	"github.com/container-registry/harbor-satellite/internal/state"
-	"github.com/container-registry/harbor-satellite/internal/utils"
 	"github.com/container-registry/harbor-satellite/pkg/config"
 )
 
 type Satellite struct {
-	schedulerKey          scheduler.SchedulerKey
-	LocalRegistryConfig   state.RegistryConfig
-	SourcesRegistryConfig state.RegistryConfig
-	UseUnsecure           bool
-	state                 string
-	cm                    *config.ConfigManager
+	schedulerKey scheduler.SchedulerKey
+	cm           *config.ConfigManager
 }
 
-func NewSatellite(ctx context.Context, schedulerKey scheduler.SchedulerKey, localRegistryConfig, sourceRegistryConfig state.RegistryConfig, cm *config.ConfigManager) *Satellite {
+func NewSatellite(schedulerKey scheduler.SchedulerKey, cm *config.ConfigManager) *Satellite {
 	return &Satellite{
-		schedulerKey:          schedulerKey,
-		LocalRegistryConfig:   localRegistryConfig,
-		SourcesRegistryConfig: sourceRegistryConfig,
-		UseUnsecure:           cm.UseUnsecure(),
-		state:                 cm.GetStateURL(),
+		schedulerKey: schedulerKey,
+		cm:           cm,
 	}
 }
 
 func (s *Satellite) Run(ctx context.Context) error {
 	log := logger.FromContext(ctx)
 	log.Info().Msg("Starting Satellite")
-	replicateStateCron := s.cm.GetStateReplicationInterval()
-	updateConfigCron := s.cm.GetUpdateConfigInterval()
-	ztrCron := s.cm.GetRegistrationInterval()
 	// Get the scheduler from the context
 	scheduler := ctx.Value(s.schedulerKey).(scheduler.Scheduler)
+
 	// Create a simple notifier and add it to the process
 	notifier := notifier.NewSimpleNotifier(ctx)
+
 	// Creating a process to fetch and replicate the state
-	fetchAndReplicateStateProcess := state.NewFetchAndReplicateStateProcess(replicateStateCron, notifier, s.SourcesRegistryConfig, s.LocalRegistryConfig, s.UseUnsecure, config.GetState())
-	configFetchProcess := state.NewFetchConfigFromGroundControlProcess(updateConfigCron, "", "")
-	ztrProcess := state.NewZtrProcess(ztrCron)
-	err := scheduler.Schedule(configFetchProcess)
-	if err != nil {
-		log.Error().Err(err).Msg("Error scheduling process")
-		return err
-	}
-	// Add the process to the scheduler
-	err = scheduler.Schedule(fetchAndReplicateStateProcess)
-	if err != nil {
-		log.Error().Err(err).Msg("Error scheduling process")
-		return err
-	}
+	fetchAndReplicateStateProcess := state.NewFetchAndReplicateStateProcess(s.cm, notifier)
+	configFetchProcess := state.NewFetchConfigFromGroundControlProcess(s.cm.GetUpdateConfigInterval(), "", "")
+	ztrProcess := state.NewZtrProcess(s.cm.GetRegistrationInterval())
 
 	// Schedule Register Satellite Process
-	if utils.IsZTRDone() {
+	if s.cm.IsZTRDone() {
 		log.Info().Msg("ZTR already performed, skipping the process")
 		return nil
 	}
 
-	err = scheduler.Schedule(ztrProcess)
+	err := scheduler.Schedule(ztrProcess)
+	if err != nil {
+		log.Error().Err(err).Msg("Error scheduling process")
+		return err
+	}
+
+	err = scheduler.Schedule(configFetchProcess)
+	if err != nil {
+		log.Error().Err(err).Msg("Error scheduling process")
+		return err
+	}
+
+	// Add the process to the scheduler
+	err = scheduler.Schedule(fetchAndReplicateStateProcess)
 	if err != nil {
 		log.Error().Err(err).Msg("Error scheduling process")
 		return err
