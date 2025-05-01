68be39189beab5db3855c1762bd722383c077891
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-05-01 19:47:26 +0530

    refactor group handlers

--- ground-control/internal/server/group_handlers.go
+++ ground-control/internal/server/group_handlers.go
@@ -15,30 +15,37 @@ import (
 
 func (s *Server) groupsSyncHandler(w http.ResponseWriter, r *http.Request) {
 	var req models.StateArtifact
+
 	if err := DecodeRequestBody(r, &req); err != nil {
-		log.Println(err)
+		log.Println("Error decoding request body:", err)
 		HandleAppError(w, err)
 		return
 	}
 
-	// Start a new transaction
 	tx, err := s.db.BeginTx(r.Context(), nil)
 	if err != nil {
-		log.Println(err)
+		log.Println("Could not begin transaction:", err)
 		HandleAppError(w, err)
 		return
 	}
-	// Create a new Queries object bound to the transaction
-	q := s.dbQueries.WithTx(tx)
-	// Ensure proper transaction handling with defer
+
+	committed := false
 	defer func() {
-		if p := recover(); p != nil {
-			// If there's a panic, rollback the transaction
-			tx.Rollback()
-		} else if err != nil {
-			tx.Rollback() // Rollback transaction on error
+		if !committed {
+			if err := tx.Rollback(); err != nil {
+				log.Printf("Error: Failed to rollback transaction for failed process: %v", err)
+				err := &AppError{
+					Message: "Error: Failed to rollback transaction",
+					Code:    http.StatusInternalServerError,
+				}
+				HandleAppError(w, err)
+				return
+			}
 		}
 	}()
+
+	q := s.dbQueries.WithTx(tx)
+
 	projects := utils.GetProjectNames(&req.Artifacts)
 	params := database.CreateGroupParams{
 		GroupName:   req.Group,
@@ -47,13 +54,14 @@ func (s *Server) groupsSyncHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	result, err := q.CreateGroup(r.Context(), params)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error creating group:", err)
 		HandleAppError(w, err)
 		return
 	}
+
 	satellites, err := q.GroupSatelliteList(r.Context(), result.ID)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error listing group satellites:", err)
 		HandleAppError(w, err)
 		return
 	}
@@ -61,54 +69,60 @@ func (s *Server) groupsSyncHandler(w http.ResponseWriter, r *http.Request) {
 	for _, satellite := range satellites {
 		robotAcc, err := q.GetRobotAccBySatelliteID(r.Context(), satellite.SatelliteID)
 		if err != nil {
-			log.Println(err)
+			log.Println("Error getting robot account by satellite ID:", err)
 			HandleAppError(w, err)
 			return
 		}
-		// update robot account projects permission
+
 		_, err = utils.UpdateRobotProjects(r.Context(), projects, robotAcc.RobotID)
 		if err != nil {
-			log.Println(err)
+			log.Println("Error updating robot projects:", err)
 			HandleAppError(w, err)
 			return
 		}
 	}
 
-	// check if project satellite exists and if does not exist create project satellite
 	satExist, err := harbor.GetProject(r.Context(), "satellite")
 	if err != nil {
-		log.Println(err)
+		log.Println("Error checking satellite project existence:", err)
 		err := &AppError{
 			Message: fmt.Sprintf("Error: Checking satellite project: %v", err),
 			Code:    http.StatusBadGateway,
 		}
 		HandleAppError(w, err)
-		tx.Rollback()
 		return
 	}
+
 	if !satExist {
 		_, err := harbor.CreateSatelliteProject(r.Context())
 		if err != nil {
-			log.Println(err)
+			log.Println("Error creating satellite project:", err)
 			err := &AppError{
 				Message: fmt.Sprintf("Error: creating satellite project: %v", err),
 				Code:    http.StatusBadGateway,
 			}
 			HandleAppError(w, err)
-			tx.Rollback()
 			return
 		}
 	}
 
-	// Create State Artifact for the group
 	err = utils.CreateStateArtifact(r.Context(), &req)
 	if err != nil {
-		log.Println(err)
+		log.Println("Error creating state artifact:", err)
 		HandleAppError(w, err)
 		return
 	}
 
-	tx.Commit()
+	if err := tx.Commit(); err != nil {
+		log.Printf("Commit failed: %v", err)
+		HandleAppError(w, &AppError{
+			Message: "Error: Could not commit transaction",
+			Code:    http.StatusInternalServerError,
+		})
+		return
+	}
+	committed = true
+
 	WriteJSONResponse(w, http.StatusOK, result)
 }
 
@@ -118,6 +132,7 @@ func (s *Server) getGroupHandler(w http.ResponseWriter, r *http.Request) {
 
 	result, err := s.dbQueries.GetGroupByName(r.Context(), group)
 	if err != nil {
+        log.Printf("Could not get group: ", err)
 		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
@@ -128,6 +143,7 @@ func (s *Server) getGroupHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) listGroupHandler(w http.ResponseWriter, r *http.Request) {
 	result, err := s.dbQueries.ListGroups(r.Context())
 	if err != nil {
+        log.Printf("Could not list groups: ", err)
 		HandleAppError(w, err)
 		return
 	}
