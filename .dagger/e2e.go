2014d705aab5cb9a550daa67d854919554614222
bupd <bupdprasanth@gmail.com>
2025-08-12 18:20:58 +0530

    feat: remove dockerhub and move to registry.goharbor.io
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/e2e.go
+++ .dagger/e2e.go
@@ -20,8 +20,8 @@ const (
 	harborAdminPassword = "Harbor12345"
 
 	harborImageTag   = "satellite"
-	postgresImage    = "goharbor/harbor-db:dev" 
-	redisImage       = "goharbor/redis-photon:dev"
+	postgresImage    = "registry.goharbor.io/dockerhub/goharbor/harbor-db:dev" 
+	redisImage       = "registry.goharbor.io/dockerhub/goharbor/redis-photon:dev"
 	registryImage    = "registry.goharbor.io/harbor-next/harbor-registry:" + harborImageTag
 	registryCtlImage = "registry.goharbor.io/harbor-next/harbor-registryctl:" + harborImageTag
 	coreImage        = "registry.goharbor.io/harbor-next/harbor-core:" + harborImageTag
