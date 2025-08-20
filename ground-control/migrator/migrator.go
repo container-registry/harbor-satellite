955c449a53dadee15199dca7f075434857208b2e
bupd <bupdprasanth@gmail.com>
2025-08-20 21:58:02 +0530

    fix: db-migrator in ground control
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- ground-control/migrator/migrator.go
+++ ground-control/migrator/migrator.go
@@ -3,9 +3,8 @@ package migrator
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	"log"
-	"net"
-	"net/url"
 	"os"
 	"time"
 
@@ -19,26 +18,28 @@ type DBConfig struct {
 	Port string
 }
 
+var (
+	dbName   = os.Getenv("DB_DATABASE")
+	password = os.Getenv("DB_PASSWORD")
+	username = os.Getenv("DB_USERNAME")
+	PORT     = os.Getenv("DB_PORT")
+	HOST     = os.Getenv("DB_HOST")
+)
+
 func parseDBConfig() DBConfig {
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL == "" {
-		log.Fatal("DATABASE_URL is required")
-	}
-
-	u, err := url.Parse(dbURL)
-	if err != nil {
-		log.Fatalf("invalid DATABASE_URL: %v", err)
-	}
-
-	host, port, err := net.SplitHostPort(u.Host)
-	if err != nil {
-		log.Fatalf("missing or invalid port in DATABASE_URL: %v", err)
-	}
+	dbURL := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		username,
+		password,
+		HOST,
+		PORT,
+		dbName,
+	)
 
 	return DBConfig{
 		URL:  dbURL,
-		Host: host,
-		Port: port,
+		Host: HOST,
+		Port: PORT,
 	}
 }
 
