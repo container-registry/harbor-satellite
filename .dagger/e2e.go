e41750f273f23062fce0c0c9865a05e48810187e
bupd <bupdprasanth@gmail.com>
2026-01-13 15:52:24 +0530

    fix(e2e): use stable v2.14.0 images for postgres and redis
    Replace dev tags with v2.14.0 for harbor-db and redis-photon only.
    Other harbor components continue using harbor-next satellite tag.
    
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- .dagger/e2e.go
+++ .dagger/e2e.go
@@ -19,13 +19,13 @@ const (
 	harborAdminUser     = "admin"
 	harborAdminPassword = "Harbor12345"
 
-	harborImageTag   = "v2.14.0"
-	postgresImage    = "goharbor/harbor-db:" + harborImageTag
-	redisImage       = "goharbor/redis-photon:" + harborImageTag
-	registryImage    = "goharbor/harbor-registry:" + harborImageTag
-	registryCtlImage = "goharbor/harbor-registryctl:" + harborImageTag
-	coreImage        = "goharbor/harbor-core:" + harborImageTag
-	jobImage         = "goharbor/harbor-jobservice:" + harborImageTag
+	harborImageTag   = "satellite"
+	postgresImage    = "registry.goharbor.io/dockerhub/goharbor/harbor-db:v2.14.0"
+	redisImage       = "registry.goharbor.io/dockerhub/goharbor/redis-photon:v2.14.0"
+	registryImage    = "registry.goharbor.io/harbor-next/harbor-registry:" + harborImageTag
+	registryCtlImage = "registry.goharbor.io/harbor-next/harbor-registryctl:" + harborImageTag
+	coreImage        = "registry.goharbor.io/harbor-next/harbor-core:" + harborImageTag
+	jobImage         = "registry.goharbor.io/harbor-next/harbor-jobservice:" + harborImageTag
 
 	configDirPath = "./test/e2e/testconfig/config/"
 
