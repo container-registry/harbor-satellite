fbbc801acdf2b1a92c6f0b70147e67a1e41789d0
Aditya Chopra <adityachopra2912@gmail.com>
2026-02-16 11:00:00 +0530

    feat: create client for gcctl
    Signed-off-by: Aditya Chopra <adityachopra2912@gmail.com>

---
+++ ground-control/cmd/gcctl/pkg/api/client.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type Client struct {
+	BaseURL string
+	Token   string
+	HTTP    *http.Client
+}
+
+func NewClient(baseURL, token string) *Client {
+	return &Client{
+		BaseURL: strings.TrimRight(baseURL, "/"),
+		Token:   token,
+		HTTP: &http.Client{
+			Timeout: 30 * time.Second,
+		},
+	}
+}
+
+type APIError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("API error (HTTP %d): %s", e.StatusCode, e.Message)
+}
