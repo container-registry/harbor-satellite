99a33a9f50b8683b85b46f7c052bdffdb2571a3a
bupd <bupdprasanth@gmail.com>
2026-02-19 08:55:18 +0530

    fix: check resp.Body.Close error in zot storage test
    Signed-off-by: bupd <bupdprasanth@gmail.com>

--- internal/registry/zot_storage_test.go
+++ internal/registry/zot_storage_test.go
@@ -55,7 +55,11 @@ func startZotRegistry(t *testing.T, storageDir string) string {
 		if err != nil {
 			return false
 		}
-		resp.Body.Close()
+		defer func() {
+			if err := resp.Body.Close(); err != nil {
+				t.Logf("error closing response body: %v", err)
+			}
+		}()
 		return resp.StatusCode == http.StatusOK
 	}, 5*time.Second, 50*time.Millisecond, "/v2/ not ready")
 
