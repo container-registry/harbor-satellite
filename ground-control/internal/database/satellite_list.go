535762d6352438e1ddbb5eb42c2176e462f376a8
bhuvan-somisetty <somisettybhuvan5@gmail.com>
2026-05-06 20:33:46 +0530

    fix: check rows.Close error in ListSatellitesFiltered
    Signed-off-by: bhuvan-somisetty <somisettybhuvan5@gmail.com>

--- ground-control/internal/database/satellite_list.go
+++ ground-control/internal/database/satellite_list.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 )
@@ -51,7 +52,6 @@ LIMIT $%d OFFSET $%d`, whereSQL, sortColumn, order, limitArg, offsetArg)
 	if err != nil {
 		return nil, 0, err
 	}
-	defer rows.Close()
 
 	items := make([]Satellite, 0)
 	for rows.Next() {
@@ -64,7 +64,7 @@ LIMIT $%d OFFSET $%d`, whereSQL, sortColumn, order, limitArg, offsetArg)
 			&i.LastSeen,
 			&i.HeartbeatInterval,
 		); err != nil {
-			return nil, 0, err
+			return nil, 0, errors.Join(err, rows.Close())
 		}
 		items = append(items, i)
 	}
