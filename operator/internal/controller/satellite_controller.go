8ed89e9eac4a3867f00c1519b1d3280b72a22597
bhuvan-somisetty <somisettybhuvan5@gmail.com>
2026-05-11 17:38:47 +0530

    feat: add finalizer to Satellite CRD for Helm cleanup on delete
    Signed-off-by: bhuvan-somisetty <somisettybhuvan5@gmail.com>

--- operator/internal/controller/satellite_controller.go
+++ operator/internal/controller/satellite_controller.go
@@ -8,9 +8,12 @@ import (
 	"helm.sh/helm/v3/pkg/chart/loader"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	fleetv1alpha1 "github.com/container-registry/harbor-satellite/operator/api/v1alpha1"
 )
+
+const finalizerName = "fleet.harbor-satellite.io/gc-deregister"
 
 // SatelliteReconciler reconciles Satellite objects against Ground Control.
 type SatelliteReconciler struct {
@@ -24,10 +27,33 @@ func (r *SatelliteReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	if !sat.DeletionTimestamp.IsZero() {
+		return r.reconcileDelete(ctx, &sat)
+	}
+
+	if !controllerutil.ContainsFinalizer(&sat, finalizerName) {
+		controllerutil.AddFinalizer(&sat, finalizerName)
+		if err := r.Update(ctx, &sat); err != nil {
+			return ctrl.Result{}, fmt.Errorf("add finalizer: %w", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
 	if err := r.reconcileHelmRelease(ctx, &sat); err != nil {
 		return ctrl.Result{}, err
 	}
 
+	return ctrl.Result{}, nil
+}
+
+func (r *SatelliteReconciler) reconcileDelete(ctx context.Context, sat *fleetv1alpha1.Satellite) (ctrl.Result, error) {
+	if err := r.uninstallRelease(sat); err != nil {
+		return ctrl.Result{}, err
+	}
+	controllerutil.RemoveFinalizer(sat, finalizerName)
+	if err := r.Update(ctx, sat); err != nil {
+		return ctrl.Result{}, fmt.Errorf("remove finalizer: %w", err)
+	}
 	return ctrl.Result{}, nil
 }
 
@@ -81,6 +107,21 @@ func (r *SatelliteReconciler) upgradeRelease(cfg *action.Configuration, sat *fle
 
 	if _, err = upgrade.Run(sat.Name, ch, nil); err != nil {
 		return fmt.Errorf("helm upgrade: %w", err)
+	}
+	return nil
+}
+
+func (r *SatelliteReconciler) uninstallRelease(sat *fleetv1alpha1.Satellite) error {
+	cfg := new(action.Configuration)
+	if err := cfg.Init(nil, sat.Namespace, r.HelmDriver, func(format string, v ...any) {
+		ctrl.Log.Info(fmt.Sprintf(format, v...))
+	}); err != nil {
+		return fmt.Errorf("init helm config: %w", err)
+	}
+
+	uninstall := action.NewUninstall(cfg)
+	if _, err := uninstall.Run(sat.Name); err != nil {
+		return fmt.Errorf("helm uninstall: %w", err)
 	}
 	return nil
 }
