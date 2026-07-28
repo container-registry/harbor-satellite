6f6f221f23c3d9f92ae92f1c4e403d48757469c8
bhuvan-somisetty <somisettybhuvan5@gmail.com>
2026-07-28 09:58:28 +0530

    fix: use log.Print instead of zerolog.Ctx in config handlers
    #448 doesn't actually attach a zerolog logger to the request context,
    so zerolog.Ctx(r.Context()) hits the disabled default logger and
    errors get silently dropped. Back to log.Print until that's wired up.
    
    Signed-off-by: bhuvan-somisetty <somisettybhuvan5@gmail.com>

--- ground-control/internal/server/config_handlers.go
+++ ground-control/internal/server/config_handlers.go
@@ -16,7 +16,6 @@ import (
 	jsonpatch "github.com/evanphx/json-patch"
 	"github.com/gorilla/mux"
 	"github.com/lib/pq"
-	"github.com/rs/zerolog"
 )
 
 type SatelliteConfigParams struct {
@@ -253,7 +252,7 @@ func (s *Server) updateConfigHandler(w http.ResponseWriter, r *http.Request) {
 func (s *Server) listConfigsHandler(w http.ResponseWriter, r *http.Request) {
 	result, err := s.dbQueries.ListConfigs(r.Context())
 	if err != nil {
-		zerolog.Ctx(r.Context()).Error().Err(err).Msg("could not list configs")
+		log.Println("could not list configs: ", err)
 		HandleAppError(w, err)
 		return
 	}
@@ -267,7 +266,7 @@ func (s *Server) getConfigHandler(w http.ResponseWriter, r *http.Request) {
 
 	result, err := s.dbQueries.GetConfigByName(r.Context(), configName)
 	if err != nil {
-		zerolog.Ctx(r.Context()).Error().Err(err).Str("config", configName).Msg("could not get config")
+		log.Printf("could not get config %q: %v", configName, err)
 		HandleAppError(w, &AppError{
 			Message: fmt.Sprintf("Config not found: %v", err),
 			Code:    http.StatusNotFound,
